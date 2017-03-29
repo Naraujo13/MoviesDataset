40d54520361654cb4b5227019d14e62e522390d1
Naraujo13 <nicolas.de.araujo@hotmail.com>
2017-03-29 19:41:49 -0300

    Adicionada conversão p JSON

--- script.py
+++ script.py
@@ -5,29 +5,45 @@ from pprint import pprint
 
 
 if __name__ == "__main__":
+
+
     conn = http.client.HTTPSConnection("api.themoviedb.org")
     payload = "{}"
 
+    #Gets Connection Attributes
     conn.request("GET", "/3/configuration?api_key=287cdab5a6189edf5e200224595a40e0", payload)
     res = conn.getresponse()
-    data = res.read()
+    str_res = res.read().decode('utf-8')
+    obj = json.loads(str_res)
+    pprint(obj)
 
+    #Base URL
+    secure_base_url = obj["images"]["secure_base_url"]
+    #Poster_Size
+    poster_size = obj["images"]["poster_sizes"][3]
 
-    #breaks data - par1 = base_url, par2 = poster_size, par3 = movie_poster_url
-    base_image_url = str(data).split(",")[1].replace("secure_base_url", "").replace("\":", "").replace("\"", "")
-    #needs to be changed to get the desired poster image size
-    poster_size = "w342"
 
     movie_request_url = "/3/discover/movie?api_key=287cdab5a6189edf5e200224595a40e0&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page="
-    year = 2017
-    page = 1
+    for year in range(1980, 2017):
+        conn.request("GET", movie_request_url + "1&year=" + str(year), payload)
+        res = conn.getresponse()
+        str_res = res.read().decode('utf-8')
+        page_obj = json.loads(str_res)
+        total_pages = page_obj["total_pages"]
+        #Itera entre páginas
+        for page in range(1, total_pages):
+            conn.request("GET", movie_request_url + str(page) + "&year=" + str(year), payload)
+            res = conn.getresponse()
+            str_res = res.read().decode('utf-8')
+            page_obj = json.loads(str_res)
+            #Itera entre resultados da página
+            i = 0
+            if page_obj and page_obj["results"]:
+                for movie in page_obj["results"]:
+                    pprint(movie["title"] + " Movie " + str(i) + " - Page " + str(page))
+                    i = i + 1
 
-    #for each movie
-    conn.request("GET", movie_request_url + str(page) + "&year=" + str(year), payload)
-    res = conn.getresponse()
-    movie_data = res.read()
-    print("movie_data:")
-    print(movie_data)
+
 
     #movie_poster_url = "/rXBB8F6XpHAwci2dihBCcixIHrK.jpg"
 
