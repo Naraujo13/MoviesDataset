45bff74754f707b72b952bc6e93cb29f0f504a4d
lorenzofantunes <fantunes.lorenzo>
2017-04-20 15:52:32 -0300

    gera a lista configurada pra gerar embeddings

--- justOverview200.py
+++ justOverview200.py
@@ -9,15 +9,19 @@ for row in f:
     row = row.split(";")
     movieId = row[0]
     movieTitle = (row[1])[1:-1]
-    movieCover = row[-1]
-    movieOverview = (", ".join(row[2:-1]))[1:-1].replace(";", "")
+    movieCover = row[-1].replace("\n", "")
+    movieOverview = (", ".join(row[2:-1]))[1:-1].replace(",", "").replace("\n", "").replace("\"", "")
 
-    if(len(movieOverview) <= 200 and "no overview" not in movieOverview.lower() and movieCover != "" and movieCover != None):
+    if(len(movieOverview) <= 200 and ("no overview" not in movieOverview.lower()) and movieCover != "" and movieCover != None and len(movieCover) > 0):
         movies.append([movieId, movieTitle, movieOverview, movieCover])
 
 movies200TXT = open('dataset/movies200.txt', 'w')
 movies200CSV = open('dataset/movies200.csv', 'w')
+movies200CoverOverview = open('dataset/movies200CoverOverview.csv', 'w')
 
 for movie in movies:
     movies200TXT.write(movie[2] + "\n")
     movies200CSV.write(";".join(movie))
+    #movies200CoverOverview.write("\"poster" + movie[3] + "\";\"" + movie[2] + "\"\n")
+    movies200CoverOverview.write("poster" + movie[3] + "," + movie[2] + "\n")
+    #movies200CoverOverview.write(movie[3] + ";")
